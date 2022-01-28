43dc871dd537eb15c1565fb18e2dec5a93869c14
Eunsung Moon <eunsung9647@gmail.com>
2022-01-28 20:34:18 +0900

    header 진짜 최종

--- src/Components/AboutmeCom/Aboutme.js
+++ src/Components/AboutmeCom/Aboutme.js
@@ -4,8 +4,8 @@ import { BsCaretDownFill } from "react-icons/bs";
 export default function Aboutme() {
   return (
     <section id='aboutme'>
-      <div className='container'>
-        <h1>안녕하세요 문은성입니다. <br />
+      <div className='container aboutmeWrap'>
+        <h1 className='h1'>안녕하세요 웹 개발자 문은성입니다. <br />
           뭔가 텍스트를 넣고싶은데 오글 거려서 못넣겠다..
         </h1>
         <button>더 알아보기 <BsCaretDownFill /></button>
