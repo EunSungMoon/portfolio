6f4b41d9730cc4bcc81603cfbaee0dbce70f0f35
Eunsung Moon <eunsung9647@gmail.com>
2022-01-28 23:12:06 +0900

    about me 1차 완료

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import './App.scss';
 import Header from './Components/HeaderCom/Header';
 import Main from './Components/MainCom/Main';
 import Aboutme from "./Components/AboutmeCom/Aboutme";
+import { BrowserRouter as Router ,Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <div className='containter-fluid'>
-        <Header />
-        <Aboutme />
-        <Main />
+      <Header />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Aboutme />}/>
+          <Route path='/' element={<Main />}/>
+        </Routes>
+        </Router>
       </div>
     </div>
   );
--- src/Components/AboutmeCom/Aboutme.js
+++ src/Components/AboutmeCom/Aboutme.js
@@ -8,7 +8,7 @@ export default function Aboutme() {
         <h1 className='h1'>안녕하세요 웹 개발자 문은성입니다. <br />
           뭔가 텍스트를 넣고싶은데 오글 거려서 못넣겠다..
         </h1>
-        <button>더 알아보기 <BsCaretDownFill /></button>
+        <button className='moreBtn'>더 알아보기 <BsCaretDownFill /></button>
       </div>
     </section>
   )
