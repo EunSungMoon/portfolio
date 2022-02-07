8db6c48f65e38f0999593a396436745461053d81
Eunsung Moon <eunsung9647@gmail.com>
2022-02-07 23:26:55 +0900

    header bootstrap nav로 변경

--- src/Components/FooterCom/Footer.js
+++ src/Components/FooterCom/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
     <footer id ="footer">
       <div className='footerWrap container'>
       <div className='gitIcon'>git icon</div>
-      <p>Copyright 2022 Moon Eunsung. All rights reserved.</p>
+      <p>Copyright 2022. Moon Eunsung all rights reserved.</p>
       <p>본 사이트는 상업적 목적이 아닌 개인 포트폴리오 사이트로 제작되었습니다.</p>
       </div>
     </footer>
--- src/Components/HeaderCom/Header.js
+++ src/Components/HeaderCom/Header.js
@@ -1,15 +1,11 @@
-import { useState, useEffect } from 'react';
 import './Header.scss';
-import { HiMenu } from "react-icons/hi";
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import { useState, useEffect } from 'react';
 
 export default function Header() {
-  const [hambuger, setHamburger] = useState(true);
-  const [scrollPosition, setScrollPosition] = useState(0)
 
-  const handleHamburger = e => {
-    setHamburger(!hambuger)
-  }
-
+  const [scrollPosition, setScrollPosition] = useState(0);
   const handleScrollHeader = () => {
     setScrollPosition(window.scrollY|| document.documentElement.scrollTop)
   }
@@ -17,21 +13,24 @@ export default function Header() {
     window.addEventListener('scroll', handleScrollHeader)
   })
 
+
   return (
-    <header id="header" className={`header ${scrollPosition ? 'headerScoll' : ''}`}>
-      <div className="headerInner container">
-        <div className='h1'>Moon's Portfolio</div>
-        <nav>
-          <ol className={`tabName ${hambuger ? '' : 'active'}`}>
-            <li>About me</li>
-            <li>Skills</li>
-            <li>Project</li>
-            <li>Timeline</li>
-          </ol>
-          <div className='hamburger' onClick={handleHamburger}>
-            <HiMenu />
-          </div>
-        </nav>
+    <header id="header" className={`nav-wrapper fixed-top navbar navbar-togglealbe-sm navbar-expand-md ${scrollPosition? 'headerScroll' : ''}`}>
+      <div className="container">
+        <Navbar className='w-100' collapseOnSelect expand='lg' >
+          <Navbar.Brand href='/'>
+            <h1 className='logo'>Moon's Portfolio</h1>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+          <Navbar.Collapse id='responsive-navbar-nav'>
+            <Nav className='nav-justified w-100 nav-fill nav-name'>
+              <Nav.Link href='/'>About me</Nav.Link>
+              <Nav.Link href='/'>Skills</Nav.Link>
+              <Nav.Link href='/'>Project</Nav.Link>
+              <Nav.Link href='/'>Timeline</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
       </div>
     </header>
   )
