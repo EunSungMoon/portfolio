38da87b412328f9243d7ccd2b3b1a38612c1162c
Eunsung Moon <eunsung9647@gmail.com>
2022-06-14 15:13:12 +0900

    프렌즈 정리

--- src/Components/ProjectCom/Project.js
+++ src/Components/ProjectCom/Project.js
@@ -2,26 +2,11 @@ import './Project.scss';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import ProjectCom from './ProjectContent';
-import mainpage from './projectImg/mainpage.png'
-import boardpage from './projectImg/boardpage.png'
-import contentpage from './projectImg/contentpage.png'
+import FriendsProject from './FriendsProject';
+import friendsLogin from './projectImg/friendsLogin.png'
+
 
 export default function Project() {
-  const friendsSub = () => (
-    <p>
-      <span className='colorRed' >이번 프로젝는 처음 시도하는 것들이 많았다. Axios, react, 백엔드와의 협업까지 </span>
-      그래서 모두를 완벽하게 이해하고 넘어가기보단 새로운 것들과 친해지기에 목표를 두었다.
-      <p></p>
-      원래 프렌즈는 순수 자바스크립트로 개발을 하다가 react로 전환시켰다.
-      전환시킨 이유는 그냥이었다. 많은 사람들이 react에 열광하는지 직접 사용해보자 라는 마음으로 시작하게 된 프로젝트이다.
-      순수 자바스크립트보다 편리하게 상태관리를 할 수 있다는 장점이 가장 큰 것 같다. 처음이라 이벤트를 적용시키는 것부터 어색했다.
-      useState, useEffect를 사용하는 것, 비동기 처리되는 것은 더더욱 어려웠지만 목표대로 react와 친해지기는 성공했다.
-      <p></p>
-      백엔드와의 협업을 통해서 많은 것들을 배울 수 있는 프로젝트였다. api를 사용해보고, 다른 개발자와 소통하고, 서로의 분야에 대해 공유하는 것 등등 흥미로운 것들을 많이 배울 수 있었다.
-      <p></p>
-      앞으로 해결해 나갈 숙제들이 많지만 개발하는 내내 뿌듯하고 즐거웠던 프로젝트였다.
-    </p>
-  )
 
   const lottoSub = () => (
     <p>
@@ -40,9 +25,9 @@ export default function Project() {
 
   const marketSub = () => (
     <p>
-      typescript를 적용한 두번째 프로젝트이다. 
-      타입을 지정하는 것이 전보단 많이 좋아졌지만 그래도 익숙하지 않았다. 
-      eslint까지 적용해서인지 어려움이 많았지만 개발 공부하는데 많은 도움이 되었다. 
+      typescript를 적용한 두번째 프로젝트이다.
+      타입을 지정하는 것이 전보단 많이 좋아졌지만 그래도 익숙하지 않았다.
+      eslint까지 적용해서인지 어려움이 많았지만 개발 공부하는데 많은 도움이 되었다.
       React로 여러 프로젝트를 개발했지만 <span className='colorRed'>useState, useEffect를 사용하면서도 렌더링 되는 과정이 이해가 안됐었는데 확실하게 공부할 수 있었다.</span>
       이전보단 훨씬 다양하게 react의 기능들을 사용하려 도전하려 노력했고, 새로운 Mui 라이브러리를 사용해봤다.
     </p>
@@ -70,24 +55,7 @@ export default function Project() {
         <div className='titleWrap'>
           <h1>PROJECTS</h1>
         </div>
-        <ProjectCom
-          title="프렌즈"
-          period="2021.12-2022.02 (3인 프로젝트)"
-          projectImage={[
-            mainpage,
-            boardpage,
-            contentpage
-          ]}
-          mainDescript={`본인이 가진 기술로 봉사를 하고 싶은 미용사들과 봉사가 필요한 사람들을 연결하는 봉사활동 매칭 플랫폼입니다. 
-          기획자는 미용 자격증을 가지고 있는 사람으로 휴일에는 봉사활동에 참여하고 싶었으나 마땅한 기관이나 장소를 찾지 못하는 어려움에 착안했습니다.`}
-          subDescript={friendsSub()}
-          service="봉사 모집 게시글 작성, 수정, 삭제하기"
-          gitAddress="github.com/EunSungMoon/friends"
-          domain="곧 생길 예정"
-          front="React, javascript, sass, Bootstrap"
-          back="Django"
-          deployment="AWS, nginx"
-        />
+        <FriendsProject />
 
         <ProjectCom
           title="lottolotto"
@@ -115,9 +83,7 @@ export default function Project() {
           title="포트폴리오"
           period="2022.02- (1인 프로젝트)"
           projectImage={[
-            boardpage,
-            contentpage,
-            mainpage
+            friendsLogin
           ]}
           mainDescript={`현재 보고있는 이 웹사이트를 말하며, 포트폴리오 용으로 제작되었습니다.`}
           subDescript={portfolioSub()}
@@ -133,9 +99,7 @@ export default function Project() {
           title="똑똑이"
           period="2022.02 - 2022.03 (3인 프로젝트)"
           projectImage={[
-            boardpage,
-            contentpage,
-            mainpage
+            friendsLogin
           ]}
           mainDescript={`웹 개발 입문시 진행하는 필수 코스인  To Do 리스트 입니다. 
           회원 기능을 추가하여 로그인 된 사용자 본인의 내용만 관리할 수 있습니다. 
@@ -153,9 +117,7 @@ export default function Project() {
           title="채미왔소"
           period="2022.04 - 2022.05 (3인 프로젝트)"
           projectImage={[
-            boardpage,
-            contentpage,
-            mainpage
+            friendsLogin
           ]}
           mainDescript={`아직까지도 많은 곳에선 재고 관리를 수동으로 진행합니다. 
           이러한 문제점에 착안하여 소규모 식재료 납품 사업장에서도 전산화된  재고관리가 가능하게끔 도와주는 웹 사이트 입니다. 
