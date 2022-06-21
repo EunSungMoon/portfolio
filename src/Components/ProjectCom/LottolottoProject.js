8e444419544b3987c0ea2300f397192a05d39d21
Eunsung Moon <eunsung9647@gmail.com>
2022-06-21 15:24:07 +0900

    이미지 정리

--- src/Components/ProjectCom/LottolottoProject.js
+++ src/Components/ProjectCom/LottolottoProject.js
@@ -6,7 +6,7 @@ export default function LottolottoProject() {
   const lottoSub = () => (
     <p>
       웹 위주로 프로젝트를 진행하다가 반응형 모바일에 도전하게 되었습니다.
-      초기 디자인을 바탕으로 <span className='colorRed'>모바일을 만들었는데</span> 
+      초기 디자인을 바탕으로 <span className='colorRed'>모바일을 만들었는데 </span> 
       디자이너와 디테일한 부분들을 맞추는 과정이 흥미로웠습니다 
     </p>
   )
@@ -22,7 +22,7 @@ export default function LottolottoProject() {
       mainDescript={
         `로또 번호를 자동 생성해주는 웹사이트입니다.
         이 사이트로 1등 당첨을 기대하는 마음으로 만들었습니다. 
-        웹디자이너가 참여한 첫 프로젝트입니다.  
+        웹디자이너가 참여한 첫 프로젝트입니다.
         보라색 파스텔 톤 디자인을 채택하여 장난감 같은 편안한 분위기를 연출했습니다.
         `
       }
--- src/Components/ProjectCom/PortfoiloProject.js
+++ src/Components/ProjectCom/PortfoiloProject.js
@@ -7,7 +7,7 @@ export default function PorfoiloProject() {
       계속해서 프로젝트들을 업로드 할 예정입니다.
     </p>
   )
-  
+
   return (
     <ProjectCom
       title="포트폴리오"
@@ -15,7 +15,7 @@ export default function PorfoiloProject() {
       projectImage={[
         friendsLogin
       ]}
-      mainDescript={`현재 보고있는 이 웹사이트를 말하며, 포트폴리오 용으로 제작되었습니다.`}
+      mainDescript={`현재 보고있는 이 웹사이트를 말하며, 포트폴리오 용으로 제작되었습니다. `}
       subDescript={portfolioSub()}
       service="자기소개, 기술 스택, 프로젝트 경험, 이력"
       gitAddress="github.com/EunSungMoon/portfolio"
